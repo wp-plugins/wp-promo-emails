595291b4097f8dd9a4cd6cc332e35b9a871bc82d
cbfreeman <cbfreeman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 18:49:27 +0000

    upat vrsio
    git-svn-id: https://plugins.svn.wordpress.org/wp-promo-emails/trunk@1063313 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wppe-script.js
@@ -0,0 +1,31 @@
+ /**
+ * jQuery jPages Custom Script v0.1
+ * Client side pagination with jQuery
+ *
+ *
+ */
+   jQuery(document).ready(function() {
+    
+
+    /**
+     * Custom javascript for any div pagination
+     *
+     *
+     */
+    jQuery("div.holder").jPages({
+            containerID : "wppecontainer",
+            perPage : 12
+    });
+
+
+    jQuery("div.holder").jPages({
+    containerID : "wppe_container",
+    perPage : 12
+    });
+
+});
+
+
+  
+
+
